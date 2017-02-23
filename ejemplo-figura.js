97e67850879164f9797399cae534827d87309107
RubenAyes <rubayes93@gmail.com>
2017-02-23 13:53:39 -0600

    Update ejemplo-figura.js

--- ejemplo-figura.js
+++ ejemplo-figura.js
@@ -9,7 +9,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 300;
+camara.position.z = 100;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
