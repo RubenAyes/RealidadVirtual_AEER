ec6fc3eb969aadb4ef4cee319097171fcc51c466
RubenAyes <rubayes93@gmail.com>
2017-03-07 13:58:15 -0600

    Update ejemplo-camara.js

--- ejemplo-camara.js
+++ ejemplo-camara.js
@@ -2,7 +2,7 @@ var campoVision = 45;
 var relacionAspecto = window.innerWidth / window.innerHeight;
 var planoCercano = 1;
 var planoLejano = 1000;
-var camara = new THREE.PerspectiveCamera (campoVision, relacionAspecto, planoCercano, planoLejano);
+var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
 camara.position.z = 15;
 
 var cubo = new THREE.Mesh( new THREE.BoxGeometry( 2, 2, 2), new THREE.MeshNormalMaterial() );
