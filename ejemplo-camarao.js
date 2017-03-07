e65ab02fceddf56111b26494c953247969407a35
RubenAyes <rubayes93@gmail.com>
2017-03-07 13:57:48 -0600

    Create ejemplo-camarao.js

---
+++ ejemplo-camarao.js
@@ -0,0 +1,6 @@
+var camara = new THREE.OrtographicCamera();
+
+camara.left = window.innerWidth / -2;
+camara.right = window.innerWidth / -2;
+camara.top = window.innerHeigth / -2;
+camara.bottom = window.innerHeigth / -2;
