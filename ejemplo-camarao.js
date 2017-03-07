1cda9bb4f87860e2060bd260981c6dbd8a03c9c9
RubenAyes <rubayes93@gmail.com>
2017-03-07 14:17:27 -0600

    Update ejemplo-camarao.js

--- ejemplo-camarao.js
+++ ejemplo-camarao.js
@@ -1,6 +1,15 @@
 var camara = new THREE.OrtographicCamera();
 
 camara.left = window.innerWidth / -2;
-camara.right = window.innerWidth / -2;
-camara.top = window.innerHeigth / -2;
+camara.right = window.innerWidth / 2;
+camara.top = window.innerHeigth / 2;
 camara.bottom = window.innerHeigth / -2;
+camara.near = 0.1;
+camara.far = 1000;
+camara.updateProjectionMatrix();
+
+camara.position.z = 100;
+
+var cubo = new THREE.Mesh( new THREE.BoxGeometry(30, 30, 30 ),
+                           new THREE.MeshNormalMateral() );
+cubo.rotateY(Math.PI/4);
