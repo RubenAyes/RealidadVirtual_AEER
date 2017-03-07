f2d976e6af4a488eb1385ccc66245716ff3fdb06
RubenAyes <rubayes93@gmail.com>
2017-03-07 14:20:25 -0600

    Update ejemplo-camarao.js

--- ejemplo-camarao.js
+++ ejemplo-camarao.js
@@ -13,3 +13,20 @@ camara.position.z = 100;
 var cubo = new THREE.Mesh( new THREE.BoxGeometry(30, 30, 30 ),
                            new THREE.MeshNormalMateral() );
 cubo.rotateY(Math.PI/4);
+var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMAterial() );
+esfera1.position.x=50;
+
+var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMAterial() );
+esfera2.position.x=-50;
+esfera2.position.z=-100;
+
+var escena = new THREE.Scene();
+escena.add(esfera1);
+escena.add(esfera2);
+escena.add(cubo);
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild( renderizador.domElement );
+renderizador.render(escena, camara);
