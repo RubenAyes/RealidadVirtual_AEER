66969d314608f20d1c290b77d7170b744add9194
RubenAyes <rubayes93@gmail.com>
2017-04-18 14:06:26 -0500

    Create ilum_180417_3.js

---
+++ ilum_180417_3.js
@@ -0,0 +1,29 @@
+var iluminacion = new THREE.PointLight(0xFFFFFF);
+iluminacion.position.y = 20;
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLambertMaterial({color: "#00cc00"});
+var malla = new THREE.Mesh( forma, material);
+malla.position.y = 2;
+
+var base = new THREE.Mesh( new THREE.BoxGeometry(5,.1,5), newTHREE.MeshLambertMaterial({color: 0xFFFFFF}));
+var escena = new THREE.Scene();
+escena.add(malla);
+escena.add(base);
+escena.add(iluminacon);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 15;
+camara.position.y = 5;
+
+var renderizador = new THREE.WebGLRenderer();
+
+renderizador.setSize(window.innerWidth, window.innerHeight);
+
+renderizador.shadowMapEnabled = true;
+malla.castShadow = true;
+base.reveiveShadow = true;
+iluminacion.castShadow = true;
+
+
+document.body.appendChild( renderizador.domElement );
+renderizador.render(escena, camara);
