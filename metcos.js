101557a48066f2b453b49b18e545245bee30060e
RubenAyes <rubayes93@gmail.com>
2017-05-22 00:16:33 -0500

    Update metcos.js

--- metcos.js
+++ metcos.js
@@ -1,6 +1,6 @@
 function setup (){
 THREE.ImageUtils.crossOrigin = '';
-var textura =THREE.ImageUtils.loadTexture('RubenAyes.github.io/plasma.jpg');
+var textura =THREE.ImageUtils.loadTexture('RubenAyes.github.io/Luna.jpg');
 var material = new THREE.MeshBasicMaterial({map: textura});
 var figura = new THREE.Shape();
 
@@ -40,9 +40,7 @@ escena = new THREE.Scene();
 escena.add(malla);
 
 camara = new THREE.PerspectiveCamera();
-camara.position.z = 1010;
-camara.position.x = 500;
-camara.position.y = 600;
+
   
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
@@ -52,17 +50,17 @@ document.body.appendChild(renderer.domElement);
 function loop() {
 requestAnimationFrame(loop);
 
-malla.rotation.x += 1;
-malla.rotation.y += 1;
-
+malla.rotation.x += 0.01;
+malla.rotation.y += 0.01;
+malla.rotation.y += 0.01;
 renderer.render(escena, camara);
 }
 var camara, escena, renderer, malla;
 setup()
 loop();
 camara.position.z = 1010;
-camara.position.x = 500;
-camara.position.y = 600;
+camara.position.x = 700;
+camara.position.y = 700;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
