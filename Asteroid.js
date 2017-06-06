6ee2726690f71f6b470b72f45d2ffeb6f4935596
RubenAyes <rubayes93@gmail.com>
2017-06-06 16:46:42 -0500

    Add files via upload

---
+++ Asteroid.js
@@ -0,0 +1,223 @@
+function Asteroid(scene, scale, difficulty, viewportSize, time, audioContext, largeExplosionBuffer, mediumExplosionBuffer, smallExplosionBuffer){
+
+	try{
+		//the white line we use to draw the asteroids
+	    var asteroidMaterial = new THREE.LineBasicMaterial( { color: 0xffffff} );
+	    //holds vertices for drawing various asteroids
+	    var asteroid1Geometry = new THREE.Geometry();
+	    //the direction the asteroid is flying
+	    var xDMod = Math.random() > .5 ? 1 : -1;
+	    var yDMod = Math.random() > .5 ? 1 : -1;
+	    var asteroidVector = new THREE.Vector3(Math.random() * difficulty * 75 * xDMod, Math.random() * difficulty * 75 * yDMod, 0);
+
+	    this.position = new THREE.Vector3();
+	    this.Difficulty = difficulty;
+	    var that = this;
+
+
+	    //last time the asteroid was updated
+		var lastTime = time;
+		var timeDelta;
+		function updateTime(t){
+			if(lastTime == t) return;
+			else{
+				timeDelta = (t - lastTime) / 1000;
+				lastTime = t;
+			}
+		}
+
+		//set asteroid
+        function loadSounds(i){
+
+            var request = new XMLHttpRequest();
+            if(i == 0)request.open('GET', "Sounds/bangLarge.wav", true);
+            if(i == 1)request.open('GET', "Sounds/bangMedium.wav", true);
+            if(i == 2)request.open('GET', "Sounds/bangSmall.wav", true);
+            request.responseType = 'arraybuffer';
+
+            // Decode asynchronously
+            request.onload = function() {
+                audioContext.decodeAudioData(request.response, function(buffer) {
+                if(i == 0){
+                    largeExplosionBuffer = buffer;
+                    loadSounds(1);
+                }
+                if(i == 1){
+                    mediumExplosionBuffer = buffer;
+                    loadSounds(2);
+                }
+                if(i == 2){
+                    smallExplosionBuffer = buffer;
+                }
+                });
+            }
+            request.send();
+        }
+        if(largeExplosionBuffer == null)loadSounds(0);
+
+	    //possible asteroid geometry
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(-3,-3,0));
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(-4,0,0));
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(-3,4,0));
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(-2,3,0));
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(1,4,0));
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(4,2,0));
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(1,0,0));
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(4,-1,0));
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(2,-4,0));
+	    asteroid1Geometry.vertices.push(new THREE.Vector3(-3,-3,0));
+
+	    //create the asteroid
+	    var mainAsteroid = new THREE.Line(asteroid1Geometry, asteroidMaterial, THREE.LineStrip);
+
+	    //the bullet hitbox of the asteroid
+	    var bulletHitboxVertices = [];
+	    bulletHitboxVertices.push(new THREE.Vector3(-4 * scale,-4 * scale,0));
+	    bulletHitboxVertices.push(new THREE.Vector3(-4 * scale,4 * scale,0));
+	    bulletHitboxVertices.push(new THREE.Vector3(4 * scale,4 * scale,0));
+	    bulletHitboxVertices.push(new THREE.Vector3(4 * scale,-4 * scale,0));
+
+	    //check if any of the points collides with this hitbox
+	    this.checkCollision = function(points){
+	    	for(var i = 0; i < points.length; i ++){
+	    		//check vertical plane of hitbox
+	    		if(points[i].y < bulletHitboxVertices[1].y && points[i].y > bulletHitboxVertices[0].y ){
+	    			//check horizontal plane of the hitbox
+	    			if(points[i].x < bulletHitboxVertices[2].x && points[i].x > bulletHitboxVertices[0].x ) {
+	    				return true;
+	    			}
+	    		}
+	    	}
+	    	return false;
+	    }
+
+	    //set a random starting position
+	    this.randomStart = function(){
+	    	var startX = 0;
+	    	var startY = 0;
+	    	while(that.checkCollision([new THREE.Vector3(10, 10, 0)])){
+	    		startX = Math.random() * viewportSize;
+		    	startY = Math.random() * viewportSize;
+		    	startX = startX > viewportSize / 2 ? (startX - (viewportSize/2)) * -1 : startX;
+		    	startY = startY > viewportSize / 2 ? (startY - (viewportSize/2)) * -1 : startY;
+		    	
+		    	bulletHitboxVertices = [];
+	    		bulletHitboxVertices.push(new THREE.Vector3((-4 * scale) + startX,(-4 * scale) + startY,0));
+	    		bulletHitboxVertices.push(new THREE.Vector3((-4 * scale) + startX,(4 * scale) + startY,0));
+	    		bulletHitboxVertices.push(new THREE.Vector3((4 * scale) + startX,(4 * scale) + startY,0));
+	    		bulletHitboxVertices.push(new THREE.Vector3((4 * scale) + startX,(-4 * scale) + startY,0));
+
+		    	mainAsteroid.position.set(startX, startY, 750);
+		    	that.position.copy(mainAsteroid.position);
+	    	}
+	    	
+	    }
+
+	    //a fixed starting position
+	    this.fixedStart = function(location){
+	    	var startX = location.x;
+	    	var startY = location.y;
+
+	    	bulletHitboxVertices = [];
+    		bulletHitboxVertices.push(new THREE.Vector3((-4 * scale) + startX,(-4 * scale) + startY,0));
+    		bulletHitboxVertices.push(new THREE.Vector3((-4 * scale) + startX,(4 * scale) + startY,0));
+    		bulletHitboxVertices.push(new THREE.Vector3((4 * scale) + startX,(4 * scale) + startY,0));
+    		bulletHitboxVertices.push(new THREE.Vector3((4 * scale) + startX,(-4 * scale) + startY,0));
+
+	    	mainAsteroid.position.set(startX, startY, 750);
+	    	if(isNaN(mainAsteroid.position.x)) alert("bFUCK");
+	    	that.position.copy(mainAsteroid.position);
+	    	if(isNaN(that.position.x)) alert("cFUCK");
+	    }
+	    
+
+	    //set asteroid scale
+	    if(isNaN(mainAsteroid.position.x)) alert("aFUCK");
+	    mainAsteroid.scale.set(scale,scale,1);
+	    scene.add(mainAsteroid);
+
+
+	    function setHitboxXY(xValue, yValue){
+	    	bulletHitboxVertices = [];
+			bulletHitboxVertices.push(new THREE.Vector3((-4 * scale) + xValue,(-4 * scale) + yValue,0));
+			bulletHitboxVertices.push(new THREE.Vector3((-4 * scale) + xValue,(4 * scale) + yValue,0));
+			bulletHitboxVertices.push(new THREE.Vector3((4 * scale) + xValue,(4 * scale) + yValue,0));
+			bulletHitboxVertices.push(new THREE.Vector3((4 * scale) + xValue,(-4 * scale) + yValue,0));
+	    }
+
+	    this.draw = function(t){
+
+	    	updateTime(t);
+	    	var vec = new THREE.Vector3();
+	    	vec.copy(asteroidVector);
+	    	vec.multiplyScalar(timeDelta);
+	    	mainAsteroid.position.add(vec);
+	    	that.position.copy(mainAsteroid.position);
+
+	    	//check to see if we went 
+	    	if(mainAsteroid.position.x < viewportSize / -2){
+	            mainAsteroid.position.x = viewportSize / 2;
+	        }
+	        if(mainAsteroid.position.x > viewportSize / 2){ 
+	            mainAsteroid.position.x = viewportSize / -2;
+	        }
+	        if(mainAsteroid.position.y < viewportSize / -2){ 
+	            mainAsteroid.position.y = viewportSize / 2;
+	        }
+	        if(mainAsteroid.position.y > viewportSize / 2){ 
+	            mainAsteroid.position.y = viewportSize / -2;
+	        }
+
+	        setHitboxXY(mainAsteroid.position.x, mainAsteroid.position.y);
+	    }
+
+	    this.destroy = function(sound){
+	    	scene.remove(mainAsteroid);
+	    	var childAsteroids = [];
+
+	    	if(scale == 5){
+	    		//large explosion
+		        var explosionSource = audioContext.createBufferSource();
+		        explosionSource.buffer = largeExplosionBuffer;
+		        explosionSource.connect(audioContext.destination);
+		        if(sound)explosionSource.start(0);
+
+		        var seedDate = Date.now();
+		        for(var i = 0; i < 3; i ++){
+		            var a = new Asteroid(scene,3,that.Difficulty,viewportSize,seedDate, audioContext, largeExplosionBuffer, mediumExplosionBuffer, smallExplosionBuffer);
+		            a.fixedStart(that.position)
+		            childAsteroids.push(a);
+		        }
+	    	}
+
+	    	if(scale == 3){
+	    		//medium explosion
+		        var explosionSource = audioContext.createBufferSource();
+		        explosionSource.buffer = mediumExplosionBuffer;
+		        explosionSource.connect(audioContext.destination);
+		        if(sound)explosionSource.start(0);
+
+	    		var seedDate = Date.now();
+		        for(var i = 0; i < 3; i ++){
+		            var a = new Asteroid(scene,2,that.Difficulty,viewportSize,seedDate, audioContext, largeExplosionBuffer, mediumExplosionBuffer, smallExplosionBuffer);
+		            a.fixedStart(that.position)
+		            childAsteroids.push(a);
+		        }
+	    	}
+
+	    	if(scale == 2){
+	    		//small explosion
+		        var explosionSource = audioContext.createBufferSource();
+		        explosionSource.buffer = smallExplosionBuffer;
+		        explosionSource.connect(audioContext.destination);
+		        if(sound)explosionSource.start(0);
+	    	}
+
+	    	return childAsteroids;
+	    }
+	}
+	catch(err){
+		alert(err);
+	}
+	
+}
---
+++ Ship.js
@@ -0,0 +1,303 @@
+function SpaceShip(scene, time, viewportSize, scale, audioContext, player){
+	"use strict";
+
+
+	//the current vector for speed / direction of the spaceship
+	var shipMomentum = new THREE.Vector3(0,0,0);
+
+	var invincible = false;
+	var invincibleTime = 0;
+
+	//last time the ship was updated
+	var lastTime = time;
+	var timeDelta;
+	function updateTime(t){
+		if(lastTime == t) return;
+		else{
+			timeDelta = (t - lastTime) / 1000;
+			lastTime = t;
+		}
+	}
+
+	//whether or not we want to show exhaust
+	var showExhaust = false;
+
+	//the last time the ship fired a shot
+	var lastShot = 0;
+
+	//the white line we use to draw the spaceship
+    var shipMaterial = new THREE.LineBasicMaterial( { color: 0xffffff} );
+    //holds vertices for drawing the spaceship
+    var shipGeometry = new THREE.Geometry();
+    //holds vertices for drawing the spaceship "exhaust"
+    var exhaustGeometry = new THREE.Geometry();
+
+    //create spaceship vertices
+    shipGeometry.vertices.push(new THREE.Vector3(0,3,0));
+    shipGeometry.vertices.push(new THREE.Vector3(2,-3,0));
+    shipGeometry.vertices.push(new THREE.Vector3(1,-2,0));
+    shipGeometry.vertices.push(new THREE.Vector3(-1,-2,0));
+    shipGeometry.vertices.push(new THREE.Vector3(-2,-3,0));
+    shipGeometry.vertices.push(new THREE.Vector3(0,3,0));
+
+    //create exhaust vertices
+    exhaustGeometry.vertices.push(new THREE.Vector3(1,-2,0));
+    exhaustGeometry.vertices.push(new THREE.Vector3(0,-4,0));
+    exhaustGeometry.vertices.push(new THREE.Vector3(-1,-2,0));
+
+    //create the spaceship and exhaust
+    var spaceShip = new THREE.Line(shipGeometry, shipMaterial, THREE.LineStrip);
+    var shipExhaust = new THREE.Line(exhaustGeometry, shipMaterial, THREE.LineStrip);
+
+    //set position and scale of the spaceship / exhaust
+    spaceShip.position.set(0,0,750);
+    spaceShip.scale.set(scale,scale,1);
+    shipExhaust.position.set(0,0,0);
+    shipExhaust.scale.set(scale,scale,1);
+    
+    //add spaceship and exhaust to the scene
+    scene.add(spaceShip);
+    scene.add(shipExhaust);
+
+    //set up sounds
+    var shotBuffer = null;
+    var thrustBuffer = null;
+    function loadSounds(i){
+
+        var request = new XMLHttpRequest();
+        if(i == 0)request.open('GET', "Sounds/fire.wav", true);
+        if(i == 1)request.open('GET', "Sounds/thrust.wav", true);
+        request.responseType = 'arraybuffer';
+
+        // Decode asynchronously
+        request.onload = function() {
+            audioContext.decodeAudioData(request.response, function(buffer) {
+            if(i == 0)shotBuffer = buffer;
+            if(i == 1)thrustBuffer = buffer;
+            });
+        }
+        request.send();
+    }
+    if(player){
+        loadSounds(0);
+        loadSounds(1);
+    }
+
+    this.setPosition = function(v){
+    	spaceShip.position.set(v.x,v.y,v.z);
+    	shipExhaust.position.set(v.x,v.y,0);
+    }
+    this.setRotation = function(v){
+    	spaceShip.rotation.z = v.z;
+        shipExhaust.rotation.z = v.z;
+    }
+    this.setMomentum = function(v){
+    	shipMomentum.copy(v);
+    }
+    this.clear = function(){
+    	scene.remove(spaceShip);
+    	scene.remove(shipExhaust);
+    }
+
+    this.setInvincible = function(){
+    	invincible = true;
+    }
+    this.getInvincible = function(t){
+    	updateTime(t);
+    	if(invincible){
+    		invincibleTime += timeDelta;
+    		if(invincibleTime >= 5){
+    			invincible = false;
+    			invincibleTime = 0;
+    		}
+    	}
+    	return invincible;
+    }
+    
+
+    this.rotateLeft = function(t){
+    	updateTime(t);
+    	spaceShip.rotation.z += 6 * timeDelta;
+        shipExhaust.rotation.z += 6 * timeDelta;
+    }
+
+    this.rotateRight = function(t){
+    	updateTime(t);
+    	spaceShip.rotation.z -= 6 * timeDelta;
+        shipExhaust.rotation.z -= 6 * timeDelta;
+    }
+
+    //keep track of the last time we made the thrust sound
+    var lastThrustTime = 0;
+
+    this.goForward = function(t){
+    	updateTime(t);
+    	var forwardVector = new THREE.Vector3(0, 10 * timeDelta, 0);
+        var rotationMatrix = new THREE.Matrix4();
+        rotationMatrix.makeRotationFromEuler(spaceShip.rotation);
+        forwardVector.applyMatrix4(rotationMatrix);
+
+        //check to see if we're going too fast
+        //var newMomentum = new THREE.Vector3();
+        //newMomentum.copy(forwardVector);
+        //newMomentum.add(shipMomentum);
+        //if(newMomentum.add(shipMomentum).length() < 5){
+        	shipMomentum.add(forwardVector);
+        //}
+        
+        //thrust sound
+        if(lastTime - lastThrustTime > 250){
+            lastThrustTime = lastTime;
+            var thrustSource = audioContext.createBufferSource();
+            thrustSource.buffer = thrustBuffer;
+            thrustSource.connect(audioContext.destination);
+            thrustSource.start(0);
+        }
+
+        //make sure ship doesn't go too fast
+        if(shipMomentum.x > 5) shipMomentum.x = 5;
+        if(shipMomentum.x < -5) shipMomentum.x = -5;
+        if(shipMomentum.y > 5) shipMomentum.y = 5;
+        if(shipMomentum.y < -5) shipMomentum.y = -5;
+        //show exhaust
+        showExhaust = true;
+        shipExhaust.position.z = 1;
+    }
+
+    this.shoot = function(t, held){
+    	updateTime(t);
+
+    	//set time based on whether or not space was held
+    	var shotdelay = held ? 350 : 100;
+
+        //sounds
+        var shotSource = audioContext.createBufferSource();
+        shotSource.buffer = shotBuffer;
+        shotSource.connect(audioContext.destination);
+
+    	//check to see if enough time has elapsed since the last shot
+    	if(lastTime - lastShot > shotdelay){
+    		lastShot = lastTime;
+    		var b = new Bullet(spaceShip.position, spaceShip.rotation, scale, scene, lastTime, viewportSize);
+            shotSource.start(0);
+    		return b;
+    	}
+    	
+    }
+
+    this.draw = function(t){
+    	updateTime(t);
+
+    	//show exhaust?
+    	if(showExhaust) shipExhaust.position.z = 1;
+    	else shipExhaust.position.z = 0;
+    	showExhaust = false;
+
+    	var tempMomentum = new THREE.Vector3();
+    	tempMomentum.copy(shipMomentum);
+    	tempMomentum.multiplyScalar(50 * timeDelta);
+
+    	//ships position
+        spaceShip.position.add(tempMomentum);
+        shipExhaust.position.add(tempMomentum);
+
+        if(spaceShip.position.x < viewportSize / -2){
+            spaceShip.position.x = viewportSize / 2;
+            shipExhaust.position.x = viewportSize / 2;
+        }
+        if(spaceShip.position.x > viewportSize / 2){ 
+            spaceShip.position.x = viewportSize / -2;
+            shipExhaust.position.x = viewportSize / -2;
+        }
+        if(spaceShip.position.y < viewportSize / -2){ 
+            spaceShip.position.y = viewportSize / 2;
+            shipExhaust.position.y = viewportSize / 2;
+        }
+        if(spaceShip.position.y > viewportSize / 2){ 
+            spaceShip.position.y = viewportSize / -2;
+            shipExhaust.position.y = viewportSize / -2;
+        }
+    }
+
+    this.getVertices = function(){
+    	var verts = [];
+    	for (var i = 0; i < spaceShip.geometry.vertices.length; i ++){
+    		var Vec = new THREE.Vector3();
+    		Vec.copy(spaceShip.geometry.vertices[i]);
+    		Vec.applyAxisAngle(new THREE.Vector3(0,0,1), spaceShip.rotation.z);
+    		Vec.add(spaceShip.position);
+    		verts.push(Vec);
+    	}
+    	return verts;
+    }
+
+}
+
+function Bullet(shipLocation, shipRotation, scale, scene, time, viewportSize){
+
+	//time the bullet was created
+	var startTime = time;
+
+	//last time the bullet was updated
+	var lastTime = time;
+	var timeDelta;
+	function updateTime(t){
+		if(lastTime == t) return;
+		else{
+			timeDelta = (t - lastTime) / 1000;
+			lastTime = t;
+		}
+	}
+
+	var whiteMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } ); 
+	var bullet = new THREE.Mesh( new THREE.PlaneGeometry( 1 * scale, 1 * scale, 1, 1 ), whiteMaterial );
+    
+    //spawn bullet on ships nose
+	var noseVector = new THREE.Vector3(0,3 * scale, 0);
+	var m = new THREE.Matrix4();
+	m.makeRotationFromEuler(shipRotation);
+	noseVector.applyMatrix4(m);
+	bullet.position.copy(shipLocation);
+	bullet.position.add(noseVector);
+
+	//rotate bullet to match ships rotation
+	bullet.rotation.copy(shipRotation);
+    scene.add(bullet);
+
+    this.destroy = function(){
+    	scene.remove(bullet);
+    }
+
+    this.checkTime = function(t){
+    	updateTime(t);
+    	if(lastTime - startTime > 1000) return false;
+    	else return true;
+    }
+
+    this.move = function(){
+
+    	var movement = new THREE.Vector3(0,10,0);
+    	var m = new THREE.Matrix4();
+		m.makeRotationFromEuler(bullet.rotation);
+		movement.applyMatrix4(m);
+    	bullet.position.add(movement);
+
+    	//check to see if we went pass viewport bounds
+    	if(bullet.position.x < viewportSize / -2){
+            bullet.position.x = viewportSize / 2;
+        }
+        if(bullet.position.x > viewportSize / 2){ 
+            bullet.position.x = viewportSize / -2;
+        }
+        if(bullet.position.y < viewportSize / -2){ 
+            bullet.position.y = viewportSize / 2;
+        }
+        if(bullet.position.y > viewportSize / 2){ 
+            bullet.position.y = viewportSize / -2;
+        }
+    }
+
+    this.position = function(){
+    	return bullet.position;
+    }
+}
